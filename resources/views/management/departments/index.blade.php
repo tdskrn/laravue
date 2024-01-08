30b3426f4230e4fc7c983940ce2eb2a4fb9caf9d
Vinicius S. Rocha <viniciusbbbjj@gmail.com>
2024-01-08 10:14:38 -0300

    first commit

---
+++ resources/views/management/departments/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+    <h1>Departments</h1>
+@endsection()
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('departments/index', [DepartmentController::class, 'index'])->name('departmentsIndex');
+
+
+Route::get('users/index', [UserController::class, 'index'])->name('usersIndex');
